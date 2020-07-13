28ff3f50d056a25bc840f5ea98e3018c47541412
Daniel Villegas <dan_villegas@hotmail.com>
2020-07-13 16:50:43 -0400

    Animations Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -8,10 +8,25 @@ import { Location } from '@angular/common';
 import { switchMap } from 'rxjs/operators';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
 @Component({
     selector: 'app-dishdetail',
     templateUrl: './dishdetail.component.html',
-    styleUrls: ['./dishdetail.component.scss']
+    styleUrls: ['./dishdetail.component.scss'],
+    animations: [
+        trigger('visibility',[
+            state('shown', style({
+                transform: 'scale(1.0)',
+                opacity: 1
+            })),
+            state('hidden', style({
+                transform: 'scale(0.5)',
+                opacity: 0
+            })),
+            transition('* => *', animate('0.5s ease-in-out'))
+        ])
+    ]
 })
 export class DishdetailComponent implements OnInit {
 
@@ -22,6 +37,8 @@ export class DishdetailComponent implements OnInit {
     dishIds: string[];
     prev: string;
     next: string;
+    visibility = 'shown';
+    
 
     formErrors = {
         'author': '',
@@ -54,8 +71,9 @@ export class DishdetailComponent implements OnInit {
     ngOnInit() {
         this.dishService.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
         this.route.params
-        .pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
-        .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); },
+        .pipe(switchMap((params: Params) => { this.visibility = 'hidden';
+        return this.dishService.getDish(params['id']);}))
+        .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); this.visibility = 'shown';},
             errmess => this.errMess = <any>errmess );
     }
 
